1da549825a22bf9596404ef263901742d1d61a2c
MichaelScottBurke <micky68@gmail.com>
2021-07-26 18:13:07 -0400

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { AboutComponent } from './pages/about/about.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { ResumeComponent } from './pages/resume/resume.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { MsbNavHeaderComponent } from './msb-nav-header/msb-nav-header.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, AboutComponent, PortfolioComponent, ResumeComponent, ContactComponent ],
+  declarations: [ AppComponent, AboutComponent, PortfolioComponent, ResumeComponent, ContactComponent, MsbNavHeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/msb-nav-header/msb-nav-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-msb-nav-header',
+  templateUrl: './msb-nav-header.component.html',
+  styleUrls: ['./msb-nav-header.component.scss']
+})
+export class MsbNavHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
