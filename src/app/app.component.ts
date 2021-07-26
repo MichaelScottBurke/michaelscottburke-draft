2a63a1626fdf6e89ab6bd8edda96916ecc6e185c
MichaelScottBurke <micky68@gmail.com>
2021-07-26 18:17:07 -0400

    5 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
-  navLinks = [
-    {
-      label: 'about',
-      route: 'about'
-    },
-    {
-      label: 'resume',
-      route: 'resume'
-    },
-    {
-      label: 'portfolio',
-      route: 'portfolio'
-    },
-    {
-      label: 'contact',
-      route: 'contact'
-    }
-  ];
+
 }
--- src/app/msb-nav-header/msb-nav-header.component.ts
+++ src/app/msb-nav-header/msb-nav-header.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-msb-nav-header',
+  selector: 'msb-nav-header',
   templateUrl: './msb-nav-header.component.html',
   styleUrls: ['./msb-nav-header.component.scss']
 })
 export class MsbNavHeaderComponent implements OnInit {
+  navLinks = [
+    {
+      label: 'about',
+      route: 'about'
+    },
+    {
+      label: 'resume',
+      route: 'resume'
+    },
+    {
+      label: 'portfolio',
+      route: 'portfolio'
+    },
+    {
+      label: 'contact',
+      route: 'contact'
+    }
+  ];
 
   constructor() { }
 
