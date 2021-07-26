0731bdfd1010ffeb06b2f5526eeffec1ff0d43fb
MichaelScottBurke <micky68@gmail.com>
2021-07-26 15:51:41 -0400

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 //import { HelloComponent } from './hello.component';
 
 @NgModule({
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+/*
+import { ColorDocComponent } from '../docs/color-doc/color-doc.component';
+import { TypeDocComponent } from '../docs/type-doc/type-doc.component';
+import { ButtonsDocComponent } from '../docs/buttons-doc/buttons-doc.component';
+import { CardsDocComponent } from '../docs/cards-doc/cards-doc.component';
+import { HomeDocComponent } from '../docs/home-doc/home-doc.component';
+import { ThemeTestComponent } from '../tests/theme-test/theme-test.component';
+*/
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/theme-test', pathMatch: 'full' },
+  /*
+  { path: 'home-doc', component: HomeDocComponent },
+  { path: 'buttons-doc', component: ButtonsDocComponent },
+  { path: 'theme-test', component: ThemeTestComponent },
+  { path: 'color-doc', component: ColorDocComponent },
+  { path: 'typography-doc', component: TypeDocComponent },
+  { path: 'cards-doc', component: CardsDocComponent }
+  */
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ],
+  declarations: [
+    //ButtonsDocComponent
+  ]
+})
+export class AppRoutingModule { }
